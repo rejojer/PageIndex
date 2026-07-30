acfcde2c5844a55512886d8604086f9cfa10151f
Ray <mailtangyu@gmail.com>
2026-07-30 20:20:43 +0800

    Remove summary_model from page_index() signature

--- pageindex/page_index.py
+++ pageindex/page_index.py
@@ -1276,7 +1276,7 @@ def page_index_main(doc, opt=None):
     return asyncio.run(page_index_builder())
 
 
-def page_index(doc, model=None, summary_model=None, toc_check_page_num=None, max_page_num_each_node=None, max_token_num_each_node=None,
+def page_index(doc, model=None, toc_check_page_num=None, max_page_num_each_node=None, max_token_num_each_node=None,
                if_add_node_id=None, if_add_node_summary=None, if_add_doc_description=None, if_add_node_text=None):
     
     user_opt = {
